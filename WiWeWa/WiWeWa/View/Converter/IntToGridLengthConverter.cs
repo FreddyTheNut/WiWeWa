19b494c0468ecbf6b0444d7448a836e2636feb76
FreddyTheNut <f.erdnuess@gmail.com>
2018-03-13 21:52:35 +0100

    Added working ProgressBar

---
+++ WiWeWa/WiWeWa/View/Converter/IntToGridLengthConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WiWeWa.View.Converter
+{
+    public class IntToGridLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null)
+            {
+                int lenght = (int)value;
+                GridLength gridLength = new GridLength(lenght, GridUnitType.Star);
+
+                return gridLength;
+            }
+
+            return new GridLength(0, GridUnitType.Star);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                GridLength gridLength = (GridLength)value;
+
+                return gridLength.Value;
+            }
+
+            return 0;
+        }
+    }
+}
--- WiWeWa/WiWeWa/ViewModel/ViewViewModel/ProgressViewModel.cs
+++ WiWeWa/WiWeWa/ViewModel/ViewViewModel/ProgressViewModel.cs
@@ -3,30 +3,89 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using WiWeWa.Model.Enum;
 using WiWeWa.ViewModel.ModelViewModel;
 
 namespace WiWeWa.ViewModel.ViewViewModel
 {
-    public class ProgressViewModel
+    public class ProgressViewModel : ViewModelBase
     {
-        public ObservableCollection<FrageViewModel> Fragen { get; } = new ObservableCollection<FrageViewModel>(DatabaseViewModel.GetSelectedFragen());
+        private ObservableCollection<FrageViewModel> Fragen = new ObservableCollection<FrageViewModel>(DatabaseViewModel.GetSelectedFragen());
 
-        public Dictionary<FrageStatus, int> Progress = new Dictionary<FrageStatus, int>()
+        private int unbearbeitetCount = 1;
+        private int bearbeitetCount = 0;
+        private int richtigCount = 0;
+        private int falschCount = 0;
+
+        public int UnbearbeitetCount
         {
-            { FrageStatus.Unbearbeitet, 0 },
-            { FrageStatus.Bearbeitet, 0 },
-            { FrageStatus.Richtig, 1 },
-            { FrageStatus.Falsch, 0 }
-        };
+            get { return unbearbeitetCount; }
+            set
+            {
+                if(UnbearbeitetCount != value)
+                {
+                    unbearbeitetCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int BearbeitetCount
+        {
+            get { return bearbeitetCount; }
+            set
+            {
+                if (BearbeitetCount != value)
+                {
+                    bearbeitetCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int RichtigCount
+        {
+            get { return richtigCount; }
+            set
+            {
+                if (RichtigCount != value)
+                {
+                    richtigCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int FalschCount
+        {
+            get { return falschCount; }
+            set
+            {
+                if (FalschCount != value)
+                {
+                    falschCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ProgressViewModel()
+        {
+            Fragen.ToList().ForEach(x => x.PropertyChanged += Frage_PropertyChanged);
+
+            UpdateStatus();
+        }
+
+        private void Frage_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("Status"))
+                UpdateStatus();
+        }
 
         private void UpdateStatus()
         {
-            Progress[FrageStatus.Unbearbeitet] = Fragen.Count(x => x.Status == FrageStatus.Unbearbeitet);
-            Progress[FrageStatus.Bearbeitet] = Fragen.Count(x => x.Status == FrageStatus.Bearbeitet);
-            Progress[FrageStatus.Richtig] = Fragen.Count(x => x.Status == FrageStatus.Richtig);
-            Progress[FrageStatus.Falsch] = Fragen.Count(x => x.Status == FrageStatus.Falsch);
+            UnbearbeitetCount = Fragen.Count(x => x.Status == FrageStatus.Unbearbeitet);
+            BearbeitetCount = Fragen.Count(x => x.Status == FrageStatus.Bearbeitet);
+            RichtigCount = Fragen.Count(x => x.Status == FrageStatus.Richtig);
+            FalschCount = Fragen.Count(x => x.Status == FrageStatus.Falsch);
         }
-
     }
 }
